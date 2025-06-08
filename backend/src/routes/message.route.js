4b95de3ecf1bf34084d90308d1dcfedece86ff58
C-handan <152592546+C-handan@users.noreply.github.com>
2025-06-08 16:13:47 +0530

    typo fixed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,13 @@
-import expresss from 'express';
-import {protectRoute} from '../middleware/auth.middleware.js';
-import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message.controller.js';
-const router  = expresss.Router();
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
+const router = express.Router();
 
-
-router.get("/users", protectRoute, getUsersForSidebar) 
-router.get("/:id", protectRoute, getMessages)
-router.post("/send/:id", protectRoute, sendMessage)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 export default router;
